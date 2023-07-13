b5605cfbd72d7676ab17f0d95a92002a2bff633b
camilogs <camilo.gonzalez.col@gmail.com>
2023-07-12 19:32:52 -0500

    Implement regex utility class

--- src/main/java/org/example/Validations/UserValidator.java
+++ src/main/java/org/example/Validations/UserValidator.java
@@ -1,18 +1,16 @@
 package org.example.Validations;
 
+import org.example.Util.Regex;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserValidator {
     public Boolean validateNames(String names)throws Exception{
-        String regex = "^[a-zA-Z]+$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(names);
-
-        boolean notMatchesWithPattern = !matcher.matches();
+        boolean notMatchesPattern = !Regex.matchesPattern(names, "^[a-zA-Z]+$");
         boolean tooShortName = names.length()<10;
 
-        if (notMatchesWithPattern) {
+        if (notMatchesPattern) {
             throw new Exception("Name can´t contain numbers or symbols");
         }else if (tooShortName){
             throw new Exception("Name is too short");
