b92cb8294308c904c05cf1b14bbe65f14820017a
camilogs <camilo.gonzalez.col@gmail.com>
2023-07-12 21:12:33 -0500

    make mail validation tests pass

--- src/main/java/org/example/Validators/UserValidator.java
+++ src/main/java/org/example/Validators/UserValidator.java
@@ -3,9 +3,8 @@ package org.example.Validators;
 import org.example.Util.Regex;
 
 public class UserValidator {
-    public Boolean validateName(String names)throws Exception {
+    public Boolean validateName(String names) throws Exception {
         String regex = "^[a-zA-ZñÑ ]+$";
-
         boolean notMatchesPattern = !Regex.matchesPattern(names, regex);
         boolean tooShortName = names.length()<10;
 
@@ -14,12 +13,11 @@ public class UserValidator {
         }else if (tooShortName){
             throw new Exception("Name is too short");
         }
-
         return true;
 
     }
 
-    public Boolean validateLocation(int locationNumber)throws Exception {
+    public Boolean validateLocation(int locationNumber) throws Exception {
         boolean isHigherThan4 = locationNumber > 4;
         boolean isLowerThan1 = locationNumber < 1;
 
@@ -27,6 +25,22 @@ public class UserValidator {
             throw new Exception("Location can't be higher than 4");
         } else if (isLowerThan1) {
             throw new Exception("Location can't be lower than 1");
+        }
+        return true;
+    }
+
+    public Boolean validateMail(String mail) throws Exception {
+        String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+        boolean notMatchesPattern = !Regex.matchesPattern(mail, regex);
+        boolean doesNotStartWithNumber = !Character.isDigit(mail.charAt(0));
+        boolean doesNotContainAtSign = !mail.contains("@");
+
+        if (notMatchesPattern){
+            throw new Exception("Must be a valid mail");
+        } else if (doesNotStartWithNumber) {
+            throw new Exception("Mail can't start with number");
+        } else if (doesNotContainAtSign) {
+            throw new Exception("Mail must contain at sign");
         }
         return true;
     }
