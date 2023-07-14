50b974a23bfff0d31298529adef2bafa14745d5d
camilogs <camilo.gonzalez.col@gmail.com>
2023-07-13 19:57:21 -0500

    Refactor: Extract mail validations to another class

--- src/main/java/org/example/Validators/UserValidator.java
+++ src/main/java/org/example/Validators/UserValidator.java
@@ -28,20 +28,4 @@ public class UserValidator {
         }
         return true;
     }
-
-    public Boolean validateMail(String mail) throws Exception {
-        String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
-        boolean notMatchesPattern = !Regex.matchesPattern(mail, regex);
-        boolean startWithNumber = Character.isDigit(mail.charAt(0));
-        boolean doesNotContainAtSign = !mail.contains("@");
-
-        if (notMatchesPattern){
-            throw new Exception("Must be a valid mail");
-        } else if (startWithNumber) {
-            throw new Exception("Mail can't start with number");
-        } else if (doesNotContainAtSign) {
-            throw new Exception("Mail must contain at sign");
-        }
-        return true;
-    }
 }
