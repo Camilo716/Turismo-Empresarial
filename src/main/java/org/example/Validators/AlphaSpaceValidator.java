cb69065d9b6cb611bb51dd4a8a633c65d00b806a
camilogs <camilo.gonzalez.col@gmail.com>
2023-07-13 20:54:23 -0500

    Refactor: Extract alphanumeric with spaces validations into another class

---
+++ src/main/java/org/example/Validators/AlphaSpaceValidator.java
@@ -0,0 +1,15 @@
+package org.example.Validators;
+
+import org.example.Util.Regex;
+
+public class AlphaSpaceValidator implements IValidable<String>{
+    @Override
+    public Boolean validate(String str) throws Exception {
+        String regex = "^[a-zA-ZñÑ ]+$";
+        boolean notMatchesPattern = !Regex.matchesPattern(str, regex);
+
+        if (notMatchesPattern)
+            throw new Exception("Name can´t contain numbers or symbols");
+        return true;
+    }
+}
---
+++ src/test/java/org/ValidatorsTests/AlphaSpaceValidatorTest.java
@@ -0,0 +1,39 @@
+package org.ValidatorsTests;
+
+import org.example.Validators.AlphaSpaceValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AlphaSpaceValidatorTest {
+    private AlphaSpaceValidator alphaSpaceValidator;
+
+    @BeforeEach
+    public void setup() {
+        alphaSpaceValidator= new AlphaSpaceValidator();
+    }
+
+    @Test
+    public void testCantContainNumbers() throws Exception {
+        assertThrows(Exception.class, () -> {
+            alphaSpaceValidator.validate("CamiloGonzalez716");
+        });
+    }
+
+    @Test
+    public void testCanContainÑ() {
+        assertDoesNotThrow(() -> {
+            boolean result = alphaSpaceValidator.validate("ÑecaBikuñeta");
+            assertTrue(result);
+        });
+    }
+
+    @Test
+    public void testCanContainBlankSpaces() {
+        assertDoesNotThrow(() -> {
+            boolean result = alphaSpaceValidator.validate("Camilo Gonzalez");
+            assertTrue(result);
+        });
+    }
+}
