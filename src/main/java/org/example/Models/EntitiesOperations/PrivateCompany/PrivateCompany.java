e4cbbf549582209671960ac547bc6ddcb56f1422
camilogs <camilo.gonzalez.col@gmail.com>
2023-08-17 19:01:14 -0500

    Create PrivateCompany model

---
+++ src/main/java/org/example/Models/EntitiesOperations/PrivateCompany/PrivateCompany.java
@@ -0,0 +1,35 @@
+package org.example.Models.EntitiesOperations.PrivateCompany;
+
+import org.example.Models.AbstractEntities.Company;
+
+import java.time.LocalDate;
+
+public class PrivateCompany extends Company {
+
+    private Integer IdLegalRepresentative;
+    private String NameLegalRepresentative;
+
+    public PrivateCompany(Integer id, String nit, String name, LocalDate location,
+                          Integer idLegalRepresentative, String nameLegalRepresentative) {
+        super(id, nit, name, location);
+        IdLegalRepresentative = idLegalRepresentative;
+        NameLegalRepresentative = nameLegalRepresentative;
+    }
+
+
+    public Integer getIdLegalRepresentative() {
+        return IdLegalRepresentative;
+    }
+
+    public void setIdLegalRepresentative(Integer idLegalRepresentative) {
+        IdLegalRepresentative = idLegalRepresentative;
+    }
+
+    public String getNameLegalRepresentative() {
+        return NameLegalRepresentative;
+    }
+
+    public void setNameLegalRepresentative(String nameLegalRepresentative) {
+        NameLegalRepresentative = nameLegalRepresentative;
+    }
+}
